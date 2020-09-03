0046ad6c18412bfa35bd8d52526001c7e59d03ee
Dayron Edmundo Acosta <dayroned@gmail.com>
2020-09-02 23:47:52 -0400

    Development mail host in docker

--- config/settings/local.py
+++ config/settings/local.py
@@ -61,7 +61,7 @@ STATIC_URL = "/static/"
 
 # Email Backend
 
-EMAIL_HOST = "localhost"
+EMAIL_HOST = "django-mailhog"
 EMAIL_PORT = 1025
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
