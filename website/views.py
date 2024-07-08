f9d5933ca1dcc2a3e8bb55491273a0fa781e667a
Alex Payne <alexpayne@MacBook-Pro.local>
2024-07-08 14:44:21 -0400

    init

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.views.generic.base import TemplateView
+from django.shortcuts import render
 
 from .forms import RecaptchaAuthenticationForm
 
@@ -10,10 +11,22 @@ from .forms import RecaptchaAuthenticationForm
 class HomeView(TemplateView):
     template_name = "website/home.html"
 
+    def get(self, request, *args, **kwargs):
+        if "HX-Request" in request.headers:
+            print("HX-Request")
+            return render(request, "website/home_content.html")
+        return super().get(request, *args, **kwargs)
+
 
 # Dashboard
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = "website/dashboard.html"
+
+    def get(self, request, *args, **kwargs):
+        if "HX-Request" in request.headers:
+            print("HX-Request")
+            return render(request, "website/dashboard_content.html")
+        return super().get(request, *args, **kwargs)
 
 
 # Login
